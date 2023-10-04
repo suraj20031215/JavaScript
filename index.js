15427bc21080abba8738796763b008390b29d199
Suraj Kumar <136887316+suraj20031215@users.noreply.github.com>
2023-10-04 11:04:58 +0000

    practice

---
+++ index.js
@@ -0,0 +1,17 @@
+// var a=40;//holding number  
+// var b="Rahul";//holding string  
+
+let a = 5;
+let b=  3;
+let c= a+b;    // Sum
+let d=a-b;    // subtraction 
+let e=a*b;    // Multiply
+let f=a/b;    // Divide
+// console.log(a+b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+
+
